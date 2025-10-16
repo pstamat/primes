6ce9b295180ff90ce65c0bb710d58c78616fe470
pstamat <64311276+pstamat@users.noreply.github.com>
2025-10-16 20:31:37 +0300

    Create primes.c

---
+++ primes.c
@@ -0,0 +1,14 @@
+/* File primes.c */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+  if (argc != 3) {
+    printf("Usage: %s <minnum> <maxnum>\n", argv[0]);
+    return 1;
+  }
+
+  return 0;
+}
